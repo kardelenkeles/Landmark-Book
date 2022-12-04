7461fcc7e356eb30c84930591d566bf6ff3a409c
kardelenkeles <kar_di44@hotmail.com>
2022-12-04 15:44:22 +0300

    first commit

---
+++ app/src/main/java/com/keles/landmarkbookjava/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.keles.landmarkbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
